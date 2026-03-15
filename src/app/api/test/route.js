93c8db53dc107c36e1d818d31306469d80dadf58
Edward Alayu <edwardalayu@gmail.com>
2026-03-15 02:07:17 -0700

    Test successful, scrapes listing title and price only, gets first 3 listings

--- src/app/api/test/route.js
+++ src/app/api/test/route.js
@@ -1,23 +1,28 @@
 import { NextResponse } from "next/server";
-import { getEbayToken } from "../../../../utils/ebay";
+import { searchEbay } from "../../../../utils/ebay";
 
 // creates an actual API endpoint at http://localhost:3000/api/test
 export async function GET() {
-    // Try to get the token from eBay
-    const token = await getEbayToken();
+    // Test with this part
+    const testPart = "RTX 3060 12GB";
+    const results = await searchEbay(testPart);
 
-    // if it fails, tell us why
-    if (!token) {
-        return NextResponse.json({
-            success: false,
-            error: "Could not get token. Check your .env.local keys!"
-        }, { status: 500 });
+    if (!results) {
+        return NextResponse.json({ error: "No items found or search failed" }, { status: 500 });
     }
 
-    // If it works, show the token on the screen
+    // Grab the first listing in array
+    const firstItem = results.itemSummaries[0];
+    // Extract name of listing
+    const itemName = firstItem.title;
+    // Extract the price
+    const itemPrice = Number(firstItem.price.value);
+
+    // Send only the clean data
     return NextResponse.json({
         success: true,
-        message: "Handshake successful!",
-        your_token: token
+        part_searched: testPart,
+        found_title: itemName,
+        price_cad: itemPrice
     });
 }
