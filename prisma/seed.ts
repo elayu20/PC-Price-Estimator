783ce05531559b522423c2a7c723312f981b6d1a
Edward Alayu <edwardalayu@gmail.com>
2026-05-14 16:41:05 -0700

    Fix typescript to include built-in node type defs

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -207,7 +207,7 @@ async function main() {
     await seedInBatches(gpuData, BATCH_SIZE, (item) => {
         return prisma.part.create({
             data: {
-                name: item.name,
+                name: item.name + item.chipset,
                 brand: item.name.split(" ")[0] || "Unknown",
                 category: "GPU",
                 basePrice: item.price,
